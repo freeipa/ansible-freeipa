ccdd43f0b5b2fbf5c7e1e767fe13575ff65c686d
Thomas Woerner <twoerner@redhat.com>
2017-10-13 16:13:57 +0200

    Extended python3 test to do an additional client test
    The additional client test is needed to make sure that the client is
    installed for python3 usage. The ipalib test has not been sufficient.

---
+++ roles/ipaclient/files/py3test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# Test ipaclient python3 binding
+from ipaclient.install.client import SECURE_PATH
+
+# Check ipapython version to be >= 4.6
+from ipapython.version import NUM_VERSION, VERSION
+if NUM_VERSION < 40600:
+    raise Exception("ipa %s not usable with python3" % VERSION)
