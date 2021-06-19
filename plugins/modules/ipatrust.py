df8812fbfd06c06146644d1d0b6d874ade9d33fa
Rafael Guterres Jeffman <rjeffman@redhat.com>
2021-06-18 21:27:47 -0300

    ipatrust: Allow execution of plugins in clients hosts.
    Add a new configuration variable, `ipa_context` to module ipatrust,
    to allow an user to select the environment context the module will
    execute, server or client.
    
    A new test playbook is added:
    
        tests/trust/test_trust_client_context.yml
    
    As the test requires both server and client hosts to be available,
    the tasks are wrapped in a block that only executes if
    `group['ipaclients']` is defined through an inventory file.

--- plugins/modules/ipatrust.py
+++ plugins/modules/ipatrust.py
@@ -38,6 +38,13 @@ options:
     description:
     - Realm name
     required: true
+  ipa_context:
+    description: |
+      The context in which the module will execute. Executing in a server
+      context is preferred, use `client` to execute in a client context if
+      the server cannot be accessed.
+    choices: ["server", "client"]
+    default: server
   trust_type:
     description:
     - Trust type (ad for Active Directory, default)
@@ -179,6 +186,8 @@ def main():
             # general
             ipaadmin_principal=dict(type="str", default="admin"),
             ipaadmin_password=dict(type="str", required=False, no_log=True),
+            ipa_context=dict(type="str", required=False, default="server",
+                             choices=["server", "client"]),
             realm=dict(type="str", default=None, required=True),
             # state
             state=dict(type="str", default="present",
@@ -210,6 +219,7 @@ def main():
     ipaadmin_principal = module_params_get(
         ansible_module, "ipaadmin_principal")
     ipaadmin_password = module_params_get(ansible_module, "ipaadmin_password")
+    ipa_context = module_params_get(ansible_module, "ipa_context")
     realm = module_params_get(ansible_module, "realm")
 
     # state
@@ -235,7 +245,7 @@ def main():
         if not valid_creds(ansible_module, ipaadmin_principal):
             ccache_dir, ccache_name = temp_kinit(
                 ipaadmin_principal, ipaadmin_password)
-        api_connect()
+        api_connect(ipa_context)
         res_find = find_trust(ansible_module, realm)
 
         if state == "absent":
