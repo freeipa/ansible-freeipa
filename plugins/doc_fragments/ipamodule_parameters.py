9f0b6c2a99dd0df357db566927b8cfddf43e2397
Rafael Guterres Jeffman <rjeffman@redhat.com>
2021-10-06 15:29:22 -0300

    delete_continue: Create extension for common parameters.
    Several modules require the addition of a commom variable
    `delete_continue`. This variable should have the same interface and
    documentation in all modules.
    
    This patch provides a common, extensible, implementation to be used
    by all modules to ensure that the interface and documentation of the
    variable is the same.

---
+++ plugins/doc_fragments/ipamodule_parameters.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Authors:
+#   Rafael Guterres Jeffman <rjeffman@redhat.com>
+#
+# Copyright (C) 2021  Red Hat
+# see file 'COPYING' for use and warranty information
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+class ModuleDocFragment(object):  # pylint: disable=R0205,R0903
+    DOCUMENTATION = r""
+
+    DELETE_CONTINUE = r"""
+options:
+  delete_continue:
+    description: |
+      Continuous mode. Don't stop on errors. Valid only if `state` is `absent`.
+    aliases: ["continue"]
+    type: bool
+    default: True
+"""
