d12c3748a7e9dee1627f53da8437245d8197dbfe
Thomas Woerner <twoerner@redhat.com>
2022-11-08 13:56:42 +0100

    ipareplica_setup_ca: Fix documentation sections and agument spec
    ansible-test with ansible-2.14 is adding a lot of new tests to ensure
    that the documentation section and the agument spec is complete. Needed
    changes:
    
    DOCUMENTATION section
    
    - `type: str` needs to be set for string parameters
    - `type: list` needs to be set for list parameters
    - `elements: str` needs to be given for list of string parameters
    - `required` tags need to be fixed according to the `argument_spec`
    - `type` tag needs to match `argument_spec`
    - `author` needs to be given with the github user also: `Name (@user)`
    
    argument_spec
    
    - `type='str'` needs to be set for string parameters
    - `elements='str'` needs to be added to all list of string parameters
    
    supports_check_mode is turned off as it is not supported.
    
    A call to ansible_ipa_replica.check_imports has been added to check for import
    errors.
    
    The `copyright` date is extended with `-2022`.

--- roles/ipareplica/library/ipareplica_setup_ca.py
+++ roles/ipareplica/library/ipareplica_setup_ca.py
@@ -5,7 +5,7 @@
 #
 # Based on ipa-replica-install code
 #
-# Copyright (C) 2018  Red Hat
+# Copyright (C) 2018-2022  Red Hat
 # see file 'COPYING' for use and warranty information
 #
 # This program is free software; you can redistribute it and/or modify
@@ -40,71 +40,95 @@ description:
 options:
   pki_config_override:
     description: Path to ini file with config overrides
-    required: yes
+    type: str
+    required: no
   setup_ca:
     description: Configure a dogtag CA
-    required: yes
+    type: bool
+    required: no
   setup_kra:
     description: Configure a dogtag KRA
-    required: yes
+    type: bool
+    required: no
   no_pkinit:
     description: Disable pkinit setup steps
-    required: yes
+    type: bool
+    required: no
   subject_base:
     description:
       The certificate subject base (default O=<realm-name>).
       RDNs are in LDAP order (most specific RDN first).
-    required: no
+    type: str
+    required: yes
   ccache:
     description: The local ccache
-    required: no
+    type: str
+    required: yes
   _ca_enabled:
     description: The installer _ca_enabled setting
-    required: yes
+    type: bool
+    required: no
   _ca_file:
     description: The installer _ca_file setting
-    required: yes
+    type: str
+    required: no
   _kra_enabled:
     description: The installer _kra_enabled setting
-    required: yes
+    type: bool
+    required: no
   _kra_host_name:
     description: The installer _kra_host_name setting
-    required: yes
+    type: str
+    required: no
   _dirsrv_pkcs12_info:
     description: The installer _dirsrv_pkcs12_info setting
-    required: yes
+    type: list
+    elements: str
+    required: no
   _pkinit_pkcs12_info:
     description: The installer _pkinit_pkcs12_info setting
-    required: yes
+    type: list
+    elements: str
+    required: no
   _top_dir:
     description: The installer _top_dir setting
-    required: no
+    type: str
+    required: yes
   _ca_subject:
     description: The installer _ca_subject setting
-    required: no
+    type: str
+    required: yes
   _subject_base:
     description: The installer _subject_base setting
-    required: no
+    type: str
+    required: yes
   _random_serial_numbers:
     description: The installer _random_serial_numbers setting
+    type: bool
     required: yes
   dirman_password:
     description: Directory Manager (master) password
-    required: no
+    type: str
+    required: yes
   config_setup_ca:
     description: The config setup_ca setting
-    required: no
+    type: bool
+    required: yes
   config_master_host_name:
     description: The config master_host_name setting
-    required: no
+    type: str
+    required: yes
   config_ca_host_name:
     description: The config ca_host_name setting
-    required: no
+    type: str
+    required: yes
   config_ips:
     description: The config ips setting
-    required: yes
+    type: list
+    elements: str
+    required: no
 author:
-    - Thomas Woerner
+    - Thomas Woerner (@t-woerner)
 '''
 
 EXAMPLES = '''
@@ -117,7 +141,7 @@ import os
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.ansible_ipa_replica import (
-    AnsibleModuleLog, setup_logging, installer, DN, paths,
+    check_imports, AnsibleModuleLog, setup_logging, installer, DN, paths,
     ansible_module_get_parsed_ip_addresses,
     gen_env_boostrap_finalize_core, constants, api_bootstrap_finalize,
     gen_ReplicaConfig, gen_remote_api, api, redirect_stdout, ca,
@@ -129,35 +153,39 @@ def main():
     ansible_module = AnsibleModule(
         argument_spec=dict(
             # basic
-            pki_config_override=dict(required=False),
+            pki_config_override=dict(required=False, type='str'),
             # server
             setup_ca=dict(required=False, type='bool'),
             setup_kra=dict(required=False, type='bool'),
             no_pkinit=dict(required=False, type='bool'),
             # certificate system
-            subject_base=dict(required=True),
+            subject_base=dict(required=True, type='str'),
             # additional
-            ccache=dict(required=True),
+            ccache=dict(required=True, type='str'),
             _ca_enabled=dict(required=False, type='bool'),
-            _ca_file=dict(required=False),
+            _ca_file=dict(required=False, type='str'),
             _kra_enabled=dict(required=False, type='bool'),
-            _kra_host_name=dict(required=False),
-            _dirsrv_pkcs12_info=dict(required=False, type='list'),
-            _pkinit_pkcs12_info=dict(required=False, type='list'),
-            _top_dir=dict(required=True),
-            _ca_subject=dict(required=True),
-            _subject_base=dict(required=True),
+            _kra_host_name=dict(required=False, type='str'),
+            _dirsrv_pkcs12_info=dict(required=False, type='list',
+                                     elements='str'),
+            _pkinit_pkcs12_info=dict(required=False, type='list',
+                                     elements='str'),
+            _top_dir=dict(required=True, type='str'),
+            _ca_subject=dict(required=True, type='str'),
+            _subject_base=dict(required=True, type='str'),
             _random_serial_numbers=dict(required=True, type='bool'),
-            dirman_password=dict(required=True, no_log=True),
+            dirman_password=dict(required=True, type='str', no_log=True),
             config_setup_ca=dict(required=True, type='bool'),
-            config_master_host_name=dict(required=True),
-            config_ca_host_name=dict(required=True),
-            config_ips=dict(required=False, type='list', default=[]),
+            config_master_host_name=dict(required=True, type='str'),
+            config_ca_host_name=dict(required=True, type='str'),
+            config_ips=dict(required=False, type='list', elements='str',
+                            default=[]),
         ),
-        supports_check_mode=True,
+        supports_check_mode=False,
     )
 
     ansible_module._ansible_debug = True
+    check_imports(ansible_module)
     setup_logging()
     ansible_log = AnsibleModuleLog(ansible_module)
 
