bbefbb6f01e16e4a3856aec09de7192521013f58
Rafael Guterres Jeffman <rjeffman@redhat.com>
2023-02-10 16:31:02 -0300

    ipahbacsvcgroup: Use global member management method.
    Change ipahbacsvcgroup to use the global member management method.

--- plugins/modules/ipahbacsvcgroup.py
+++ plugins/modules/ipahbacsvcgroup.py
@@ -107,8 +107,8 @@ RETURN = """
 """
 
 from ansible.module_utils.ansible_freeipa_module import \
-    IPAAnsibleModule, compare_args_ipa, gen_add_del_lists, gen_add_list, \
-    gen_intersection_list
+    IPAAnsibleModule, compare_args_ipa, gen_member_manage_commands, \
+    create_ipa_mapping, ipa_api_map, transform_lowercase
 
 
 def find_hbacsvcgroup(module, name):
@@ -136,29 +136,6 @@ def gen_args(description, nomembers):
         _args["nomembers"] = nomembers
 
     return _args
-
-
-def gen_member_args(hbacsvc):
-    _args = {}
-    if hbacsvc is not None:
-        _args["member_hbacsvc"] = hbacsvc
-
-    return _args
-
-
-# pylint: disable=unused-argument
-def result_handler(module, result, command, name, args, errors):
-    # Get all errors
-    # All "already a member" and "not a member" failures in the
-    # result are ignored. All others are reported.
-    if "failed" in result and "member" in result["failed"]:
-        failed = result["failed"]["member"]
-        for member_type in failed:
-            for member, failure in failed[member_type]:
-                if "already a member" not in failure \
-                   and "not a member" not in failure:
-                    errors.append("%s: %s %s: %s" % (
-                        command, member_type, member, failure))
 
 
 def main():
@@ -191,7 +168,6 @@ def main():
     # present
     description = ansible_module.params_get("description")
     nomembers = ansible_module.params_get("nomembers")
-    hbacsvc = ansible_module.params_get_lowercase("hbacsvc")
     action = ansible_module.params_get("action")
     # state
     state = ansible_module.params_get("state")
@@ -231,8 +207,6 @@ def main():
             # Make sure hbacsvcgroup exists
             res_find = find_hbacsvcgroup(ansible_module, name)
 
-            hbacsvc_add, hbacsvc_del = [], []
-
             # Create command
             if state == "present":
                 # Generate args
@@ -252,23 +226,10 @@ def main():
                         # Set res_find to empty dict for next step
                         res_find = {}
 
-                    member_args = gen_member_args(hbacsvc)
-                    if not compare_args_ipa(ansible_module, member_args,
-                                            res_find):
-                        # Generate addition and removal lists
-                        if hbacsvc is not None:
-                            hbacsvc_add, hbacsvc_del = gen_add_del_lists(
-                                hbacsvc, res_find.get("member_hbacsvc"))
-
                 elif action == "member":
                     if res_find is None:
                         ansible_module.fail_json(
                             msg="No hbacsvcgroup '%s'" % name)
-
-                    # Ensure members are present
-                    if hbacsvc:
-                        hbacsvc_add = gen_add_list(
-                            hbacsvc, res_find.get("member_hbacsvc"))
 
             elif state == "absent":
                 if action == "hbacsvcgroup":
@@ -280,30 +241,31 @@ def main():
                         ansible_module.fail_json(
                             msg="No hbacsvcgroup '%s'" % name)
 
-                    # Ensure members are absent
-                    if hbacsvc:
-                        hbacsvc_del = gen_intersection_list(
-                            hbacsvc, res_find.get("member_hbacsvc"))
-
             else:
                 ansible_module.fail_json(msg="Unkown state '%s'" % state)
 
             # Manage members
-            if len(hbacsvc_add) > 0:
-                commands.append([name, "hbacsvcgroup_add_member",
-                                 {
-                                     "hbacsvc": hbacsvc_add
-                                 }])
-            # Remove members
-            if len(hbacsvc_del) > 0:
-                commands.append([name,
-                                 "hbacsvcgroup_remove_member",
-                                 {
-                                     "hbacsvc": hbacsvc_del
-                                 }])
+            commands.extend(
+                gen_member_manage_commands(
+                    ansible_module,
+                    res_find,
+                    name,
+                    "hbacsvcgroup_add_member",
+                    "hbacsvcgroup_remove_member",
+                    create_ipa_mapping(
+                        ipa_api_map(
+                            api_arg="hbacsvc",
+                            ldap_attrs="member_hbacsvc",
+                            module_params="hbacsvc",
+                            transform={"hbacsvc": transform_lowercase}
+                        )
+                    )
+                )
+            )
 
         # Execute commands
-        changed = ansible_module.execute_ipa_commands(commands, result_handler)
+        changed = ansible_module.execute_ipa_commands(
+            commands, fail_on_member_errors=True)
 
     # Done
 
