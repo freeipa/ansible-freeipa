ff8207d4f4c762bf870d891f8f90c5ea2b73e032
Rafael Guterres Jeffman <rjeffman@redhat.com>
2023-05-19 16:16:28 -0300

    fixup: several fixes, playbook, workflow.

--- tests/multihost/get_ip.sh
+++ tests/multihost/get_ip.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-INPUT=${1:--}
-
 echo "---"
-# shellcheck disable=SC2002
-cat "${INPUT}" | \
-    grep HostName -B1 | \
-    sed -e "/^--/d" \
-        -e "/^Host/N;s/\n/:/;s/Host \([a-zA-Z0-9.]*\)/\1/;s/ *HostName \(.*\)/ \1/" \
-        -e "s/server.*:/server_ip:/" \
-        -e "s/cli-.*:/client_ip:/" \
-        -e "s/rep-.*:/replica_ip:/"
+
+while [ -n "${1}" ]
+do
+    echo "${1}_ip: $(vagrant ssh -c "hostname -I" "${1}")"
+    shift
+done
